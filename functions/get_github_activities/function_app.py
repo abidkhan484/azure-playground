1a8c6e0d7489d3e2ab9c426b26a1565e688da5cb
Abid Khan <abidkhan484@gmail.com>
2023-11-21 16:41:12 +0600

    connect with cassandra implemented in azure function

--- functions/get_github_activities/function_app.py
+++ functions/get_github_activities/function_app.py
@@ -6,7 +6,7 @@ from json import loads, dumps
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="request/github")
+@app.route(route='request/github')
 def github_request(req: func.HttpRequest) -> func.HttpResponse:
     username = getenv('GITHUB_USERNAME')
     url = f'https://api.github.com/users/{username}/events/public'
@@ -31,7 +31,38 @@ def github_request(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(response)
     else:
         return func.HttpResponse(
-             "Got an error response from the GITHUB API.",
+             'Got an error response from the GITHUB API.',
+             status_code=200
+        )
+
+
+######################################################
+from cassandra.cluster import Cluster
+from ssl import PROTOCOL_TLSv1_2, SSLContext, CERT_NONE
+from cassandra.auth import PlainTextAuthProvider
+
+
+@app.route(route='connect/db', auth_level=func.AuthLevel.FUNCTION)
+def connect_with_db(req: func.HttpRequest) -> func.HttpResponse:
+    username = getenv('CASSANDRA_USERNAME')
+    password = getenv('CASSANDRA_PASSWORD')
+    domain = getenv('CASSANDRA_DOMAIN')
+    keyspace = getenv('CASSANDRA_KEYSPACE')
+
+    ssl_context = SSLContext(PROTOCOL_TLSv1_2)
+    ssl_context.verify_mode = CERT_NONE
+
+    auth_provider = PlainTextAuthProvider(username=username, password=password)
+    cluster = Cluster([domain], port = 10350, auth_provider=auth_provider, ssl_context=ssl_context)
+    session = cluster.connect(keyspace)
+
+    if session:
+        cluster.shutdown()
+        session.shutdown()
+        return func.HttpResponse('Cassandra session created succesfully.')
+    else:
+        return func.HttpResponse(
+             'Cassandra session error.',
              status_code=200
         )
 
